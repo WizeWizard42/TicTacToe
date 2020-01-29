48a9156ec5d33505366cde23569d672554dbdab8
WizeWizard42 <51838647+WizeWizard42@users.noreply.github.com>
2020-01-28 17:43:20 -0800

    Update Board.hpp

--- src/Board.hpp
+++ src/Board.hpp
@@ -1,21 +1,24 @@
-#pragma once
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
 #include <ostream>
 #include <array>
 
-class Board 
+class Board
 {
     public:
         Board();
-    
+
         char getTile(int, int);
         int setTile(int, int, char);
-        
+
         std::array<std::array<char, 3>, 3> getBoard();
-        
+
         friend std::ostream& operator<<(std::ostream&, const Board&);
-        bool checkWin(char);
     private:
         std::array<std::array<char, 3>, 3> board_;
 };
 
 std::ostream& operator<<(std::ostream&, const Board&);
+
+#endif // BOARD_HPP
